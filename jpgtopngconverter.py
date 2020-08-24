589478b6e666936556c86917cffa7e5237caba10
adigun91 <adigunjoshuaoluwafemi@gmail.com>
2020-08-24 15:43:34 +0100

    first change

---
+++ jpgtopngconverter.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from PIL import Image
+
+#grab the first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+#check if the new folder exists and if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+#loop through the folder
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    #print(clean_name)
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('All done!!!')
